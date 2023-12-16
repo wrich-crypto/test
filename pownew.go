259798c469f213be70bd81a1f80c1c123a81d1e8
wrich <36285165+wrich-crypto@users.noreply.github.com>
2023-12-16 17:14:48 +0800

    1
    1

--- pownew.go
+++ pownew.go
@@ -23,7 +23,7 @@ import (
 
 var (
 	infuraURL       = "https://eth-mainnet.g.alchemy.com/v2/FL6BaqcJywosW4svjdO4oje47zbP45Yq"
-	privateKeystr   = "私钥私钥私钥"
+	privateKeystr   = ""
 	workerCount     = 16
 	logger          = logrus.New()
 	gasTipCap       = 1000000000 // 1 Gwei 小费gas
@@ -34,7 +34,7 @@ var (
 )
 
 func getNonceDate(nonce int64) string {
-	return "data:application/json,{\"p\":\"ierc-20\",\"op\":\"mint\",\"tick\":\"electron\",\"amt\":\"54600\",\"nonce\":\"" + strconv.FormatInt(nonce, 10) + "\"}"
+	return "data:application/json,{\"p\":\"ierc-20\",\"op\":\"mint\",\"tick\":\"mice\",\"amt\":\"1000\",\"nonce\":\"" + strconv.FormatInt(nonce, 10) + "\"}"
 }
 
 var (
